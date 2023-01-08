38695c358a4117e2064a385d2de156a65d805cf3
Jagdeshkumar422 <jk9876211@gmail.com>
2023-01-08 06:42:30 -0800

    update

--- js/login.js
+++ js/login.js
@@ -52,5 +52,14 @@ let loginWithGoogle = ()=>{
 }
 
 
+let loginWithFacebook= ()=>{
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then((res)=>{
+    console.log(res)
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+
 
 
